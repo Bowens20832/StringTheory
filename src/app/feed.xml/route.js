4903bd68f865f7df712a764ce57976c85f3abf74
Brandon Owens <brandono@vitamins2you.com>
2023-08-08 19:35:50 -0400

    Initial commit

---
+++ src/app/feed.xml/route.js
@@ -0,0 +1,32 @@
+import * as cheerio from 'cheerio'
+import { Feed } from 'feed'
+
+export async function GET(req) {
+  let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
+  let author = {
+    name: 'Spencer Sharp',
+    email: 'spencer@planetaria.tech',
+  }
+
+  let feed = new Feed({
+    title: author.name,
+    description: 'Your blog description',
+    author,
+    id: siteUrl,
+    link: siteUrl,
+    image: `${siteUrl}/favicon.ico`,
+    favicon: `${siteUrl}/favicon.ico`,
+    copyright: `All rights reserved ${new Date().getFullYear()}`,
+    feedLinks: {
+      rss2: `${siteUrl}/feed.xml`,
+    },
+  })
+
+  return new Response(feed.rss2(), {
+    status: 200,
+    headers: {
+      'content-type': 'application/xml',
+      'cache-control': 's-maxage=31556952',
+    },
+  })
+}
---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Brandon Owens',
+    default:
+      'Brandon Owens -  Application engineer, entrepreneur, and amateur explorer.',
+  },
+  description:
+    "I'm Brandon, a full stack application engineer and entrepreneur based in Baltimore City, Maryland",
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
