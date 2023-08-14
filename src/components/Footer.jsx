9804785eaab2be98c11ea972cd6be5fe91e3874e
Brandon Owens <brandono@vitamins2you.com>
2023-08-14 16:24:18 -0400

    Added Elegant Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { PoweredByElegant } from '@brandonowens/elegant-ui';
 
 import { Container } from '@/components/Container'
 
@@ -33,21 +34,7 @@ export function Footer() {
             </div>
           </Container.Inner>
         </div>
-        <div className="border-t border-zinc-100 pb-8 pt-8 dark:border-zinc-700/40">
-          <div className="flex flex-row items-center justify-center text-sm font-regular text-zinc-800 dark:text-zinc-200">
-            Powered by 
-            <a 
-              target="_blank" 
-              rel="noreferrer"
-              href="http://elegantframework.com/" 
-              aria-label="Powered by the Elegant framework"
-              className="font-semibold hover:text-rose-500 dark:hover:text-rose-400 pl-1"
-            >
-                Elegant
-            </a>
-              .
-          </div>
-        </div>
+        <PoweredByElegant />
       </Container.Outer>
     </footer>
   )
